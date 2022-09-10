f7f22f6bc8d6a5e4a2393c31453e64fa73353787
Chang Ye <yech1990@gmail.com>
2022-09-09 22:13:44 -0500

    Support strand

--- src/base.rs
+++ src/base.rs
@@ -14,28 +14,36 @@ struct PosRecord {
     end: u32,
 }
 
-// fn complement_base_code(c: u8) -> u8 {
-//     // A = 65, a = 97
-//     // C = 67, c = 99
-//     // G = 71, g = 103
-//     // T = 84, t = 116
-//     // U = 85, u = 117
-//     match c {
-//         65 | 97 | 85 | 117 => 84,
-//         67 | 99 => 71,
-//         71 | 103 => 67,
-//         84 | 116 => 65,
-//         _ => 78,
-//     }
-// }
+fn complement_base_code(c: u8) -> u8 {
+    // A = 65, a = 97
+    // C = 67, c = 99
+    // G = 71, g = 103
+    // T = 84, t = 116
+    // U = 85, u = 117
+    match c {
+        65 | 97 | 85 | 117 => 84,
+        67 | 99 => 71,
+        71 | 103 => 67,
+        84 | 116 => 65,
+        _ => 78,
+    }
+}
 
 pub fn run(
     region_path: PathBuf,
     fasta_path: PathBuf,
     bam_path_list: Vec<PathBuf>,
-    min_mean_depth: u32,
+    min_depth: u32,
     count_indel: bool,
+    with_header: bool,
+    ignore_strand: bool,
+    by_strand: bool,
 ) {
+    // check parameters
+    if by_strand & ignore_strand {
+        eprintln!("Output records by strand, but `--ignore-strand` flag is set.");
+        std::process::exit(1);
+    }
     // A, C, G, T
     let dna_bases: Vec<u8> = vec![65, 67, 71, 84];
 
@@ -55,10 +63,10 @@ pub fn run(
         let start = record.start as u32;
         let end = record.end as u32;
 
-        let mut p2base: HashMap<(u32, usize), String> = HashMap::new();
-        let mut p2ins: HashMap<(u32, usize), String> = HashMap::new();
-        let mut p2del: HashMap<(u32, usize), String> = HashMap::new();
-        let mut p2depth: HashMap<(u32, usize), u32> = HashMap::new();
+        let mut p2depth: HashMap<(u32, usize), (u32, u32)> = HashMap::new();
+        let mut p2base: HashMap<(u32, usize), (Vec<usize>, Vec<usize>)> = HashMap::new();
+        let mut p2ins: HashMap<(u32, usize), (Vec<u32>, Vec<u32>)> = HashMap::new();
+        let mut p2del: HashMap<(u32, usize), (Vec<u32>, Vec<u32>)> = HashMap::new();
 
         for (i, bam_path) in bam_path_list.iter().enumerate() {
             // read bam file
@@ -74,12 +82,14 @@ pub fn run(
 
                 let mut base_list_fwd: Vec<u8> = Vec::new();
                 let mut base_list_rev: Vec<u8> = Vec::new();
-                let mut insertion_list: Vec<u32> = Vec::new();
-                let mut deletion_list: Vec<u32> = Vec::new();
+                let mut insertion_list_fwd: Vec<u32> = Vec::new();
+                let mut insertion_list_rev: Vec<u32> = Vec::new();
+                let mut deletion_list_fwd: Vec<u32> = Vec::new();
+                let mut deletion_list_rev: Vec<u32> = Vec::new();
+                let mut total_reads_fwd = 0;
+                let mut total_reads_rev = 0;
                 let ref_pos = pileup.pos();
                 if (start <= ref_pos) && (ref_pos < end) {
-                    let mut total_reads = 0;
-
                     // for alignment in pileup.alignments() {
                     // TODO: pick by quality
                     // START: group by qname
@@ -111,126 +121,224 @@ pub fn run(
                             .unwrap();
                         // END: group by qname
 
-                        if !alignment.is_del() && !alignment.is_refskip() {
-                            total_reads += 1;
-                            let read_base = alignment.record().seq()[alignment.qpos().unwrap()];
-                            if alignment.record().flags() & 128 == 128 {
-                                if alignment.record().flags() & 16 == 16 {
-                                    base_list_fwd.push(read_base);
-                                } else {
-                                    base_list_rev.push(read_base);
-                                }
+                        let strand: char;
+                        if alignment.record().flags() & 128 == 128 {
+                            if alignment.record().flags() & 16 == 16 {
+                                strand = '+'
                             } else {
-                                if alignment.record().flags() & 16 == 16 {
-                                    base_list_rev.push(read_base);
-                                } else {
-                                    base_list_fwd.push(read_base);
-                                }
+                                strand = '-'
+                            }
+                        } else {
+                            if alignment.record().flags() & 16 == 16 {
+                                strand = '-'
+                            } else {
+                                strand = '+'
                             }
                         }
-                        match alignment.indel() {
-                            bam::pileup::Indel::Ins(len) => {
-                                insertion_list.push(len);
+
+                        if !alignment.is_del() && !alignment.is_refskip() {
+                            let read_base = alignment.record().seq()[alignment.qpos().unwrap()];
+                            if strand == '+' {
+                                base_list_fwd.push(read_base);
+                                total_reads_fwd += 1;
+                            } else if strand == '-' {
+                                base_list_rev.push(read_base);
+                                total_reads_rev += 1;
+                            } else {
+                                print!("");
                             }
-                            bam::pileup::Indel::Del(len) => {
-                                deletion_list.push(len);
+                        }
+                        if count_indel {
+                            match alignment.indel() {
+                                bam::pileup::Indel::Ins(len) => {
+                                    if strand == '+' {
+                                        insertion_list_fwd.push(len);
+                                    } else if strand == '-' {
+                                        insertion_list_rev.push(len);
+                                    }
+                                }
+                                bam::pileup::Indel::Del(len) => {
+                                    if strand == '+' {
+                                        deletion_list_fwd.push(len);
+                                    } else if strand == '-' {
+                                        deletion_list_rev.push(len);
+                                    }
+                                }
+                                _ => {}
                             }
-                            _ => {}
                         }
                     }
-                    p2depth.insert((ref_pos, i), total_reads);
+                    p2depth.insert((ref_pos, i), (total_reads_fwd, total_reads_rev));
 
-                    // let base_string = String::from_utf8(base_list.clone()).unwrap();
-
-                    let mut base_counter = dna_bases
+                    let base_counter_fwd: Vec<usize>;
+                    let base_counter_rev: Vec<usize>;
+                    // count forward bases
+                    base_counter_fwd = dna_bases
                         .clone()
                         .into_iter()
-                        .map(|b| {
-                            base_list_fwd
-                                .iter()
-                                .filter(|&x| *x == b)
-                                .count()
-                                .to_string()
-                        })
+                        .map(|b| base_list_fwd.iter().filter(|&x| *x == b).count())
                         .collect::<Vec<_>>();
-                    base_counter.append(
-                        &mut dna_bases
-                            .clone()
-                            .into_iter()
-                            .map(|b| {
-                                base_list_rev
-                                    .iter()
-                                    .filter(|&x| *x == b)
-                                    .count()
-                                    .to_string()
-                            })
-                            .collect::<Vec<_>>(),
-                    );
-                    p2base.insert((ref_pos, i), base_counter.join(","));
+                    // count reverse bases
+                    base_counter_rev = dna_bases
+                        .clone()
+                        .into_iter()
+                        .map(|b| base_list_rev.iter().filter(|&x| *x == b).count())
+                        .collect::<Vec<_>>();
+
+                    p2base.insert((ref_pos, i), (base_counter_fwd, base_counter_rev));
 
                     if count_indel {
-                        let insertion_counter = insertion_list
-                            .into_iter()
-                            .map(|x| x.to_string())
-                            .collect::<Vec<_>>()
-                            .join("|");
-                        p2ins.insert((ref_pos, i), insertion_counter);
-
-                        let deletion_counter = deletion_list
-                            .into_iter()
-                            .map(|x| x.to_string())
-                            .collect::<Vec<_>>()
-                            .join("|");
-                        p2del.insert((ref_pos, i), deletion_counter);
+                        p2ins.insert((ref_pos, i), (insertion_list_fwd, insertion_list_rev));
+                        p2del.insert((ref_pos, i), (deletion_list_fwd, deletion_list_rev));
                     }
                 }
             }
         }
 
+        if with_header {
+            print!("Chrom\tPos\tStrand\tRef");
+            for pth in &bam_path_list {
+                print!("\t{}", pth.to_str().unwrap().to_string())
+            }
+            print!("\n");
+        }
         for p in start..end {
-            // filter depth
-            let dep: u32 = (0..bam_path_list.len())
-                .map(|x| match p2depth.get(&(p, x)) {
-                    Some(val) => *val,
-                    None => 0,
+            let rec_list = (0..bam_path_list.len())
+                .map(|x| {
+                    if ignore_strand {
+                        let mut rec = vec![match p2base.get(&(p, x)) {
+                            Some((v1, v2)) => format!(
+                                "{}",
+                                (0..4)
+                                    .map(|i| v1[i] + v2[i])
+                                    .collect::<Vec<usize>>()
+                                    .iter()
+                                    .join(",")
+                            ),
+                            None => "0,0,0,0".to_string(),
+                        }];
+                        if count_indel {
+                            rec.push(match p2ins.get(&(p, x)) {
+                                Some((v1, v2)) => {
+                                    format!("{}", v1.iter().chain(v2.iter()).join("|"))
+                                }
+                                None => "".to_string(),
+                            });
+                            rec.push(match p2del.get(&(p, x)) {
+                                Some((v1, v2)) => {
+                                    format!("{}", v1.iter().chain(v2.iter()).join("|"))
+                                }
+                                None => "".to_string(),
+                            });
+                        }
+                        vec![rec.join(",")]
+                    } else if by_strand {
+                        let mut rec = match p2base.get(&(p, x)) {
+                            Some((v1, v2)) => vec![v1.iter().join(","), v2.iter().join(",")],
+                            None => vec!["0,0,0,0".to_string(), "0,0,0,0".to_string()],
+                        };
+                        if count_indel {
+                            rec.append(&mut match p2ins.get(&(p, x)) {
+                                Some((v1, v2)) => vec![v1.iter().join("|"), v2.iter().join("|")],
+                                None => vec!["".to_string(), "".to_string()],
+                            });
+                            rec.append(&mut match p2del.get(&(p, x)) {
+                                Some((v1, v2)) => vec![v1.iter().join("|"), v2.iter().join("|")],
+                                None => vec!["".to_string(), "".to_string()],
+                            });
+                        }
+                        vec![
+                            rec.iter().step_by(2).join(","),
+                            rec.iter().step_by(2).join(","),
+                        ]
+                    } else {
+                        let mut rec = vec![match p2base.get(&(p, x)) {
+                            Some((v1, v2)) => {
+                                format!("{},{}", v1.iter().join(","), v2.iter().join(","))
+                            }
+                            None => "0,0,0,0,0,0,0,0".to_string(),
+                        }];
+                        if count_indel {
+                            rec.push(match p2ins.get(&(p, x)) {
+                                Some((v1, v2)) => {
+                                    format!("{}|{}", v1.iter().join("|"), v2.iter().join("|"))
+                                }
+                                None => "".to_string(),
+                            });
+                            rec.push(match p2del.get(&(p, x)) {
+                                Some((v1, v2)) => {
+                                    format!("{}|{}", v1.iter().join("|"), v2.iter().join("|"))
+                                }
+                                None => "".to_string(),
+                            });
+                        }
+                        vec![rec.join(",")]
+                    }
                 })
-                .sum();
+                .collect::<Vec<_>>();
 
             let r = fa_reader
                 .fetch_seq_string(&record.chrom, p as usize, p as usize)
                 .unwrap();
-            if (dep / bam_path_list.len() as u32) >= min_mean_depth {
-                print!("{}\t{}\t{}\t", record.chrom, p, r);
-                let val = (0..bam_path_list.len())
-                    .map(|x| {
-                        if count_indel {
-                            format!(
-                                "{},{},{}",
-                                match p2base.get(&(p, x)) {
-                                    Some(val) => val,
-                                    None => "0,0,0,0,0,0,0,0",
-                                },
-                                match p2ins.get(&(p, x)) {
-                                    Some(val) => val,
-                                    None => "",
-                                },
-                                match p2del.get(&(p, x)) {
-                                    Some(val) => val,
-                                    None => "",
-                                }
-                            )
-                        } else {
-                            match p2base.get(&(p, x)) {
-                                Some(val) => val,
-                                None => "0,0,0,0,0,0,0,0",
-                            }
-                            .to_string()
-                        }
+            if ignore_strand {
+                // filter depth
+                let passed_filter = (0..bam_path_list.len())
+                    .map(|x| match p2depth.get(&(p, x)) {
+                        Some(val) => (*val).0 + (*val).1,
+                        None => 0,
                     })
-                    .collect::<Vec<_>>()
-                    .join("\t");
-
-                println!("{}", val);
+                    .max()
+                    .unwrap()
+                    >= min_depth;
+                if passed_filter {
+                    let val = rec_list.iter().map(|x| &x[0]).join("\t");
+                    println!("{}\t{}\t{}\t{}\t{}", record.chrom, p, '.', r, val);
+                }
+            } else if by_strand {
+                let passed_filter = (0..bam_path_list.len())
+                    .map(|x| match p2depth.get(&(p, x)) {
+                        Some(val) => (*val).0,
+                        None => 0,
+                    })
+                    .max()
+                    .unwrap()
+                    >= min_depth;
+                if passed_filter {
+                    let val = rec_list.iter().map(|x| &x[0]).join("\t");
+                    println!("{}\t{}\t{}\t{}\t{}", record.chrom, p, "+", r, val);
+                }
+                let passed_filter = (0..bam_path_list.len())
+                    .map(|x| match p2depth.get(&(p, x)) {
+                        Some(val) => (*val).1,
+                        None => 0,
+                    })
+                    .max()
+                    .unwrap()
+                    >= min_depth;
+                if passed_filter {
+                    let val = rec_list.iter().map(|x| &x[1]).join("\t");
+                    println!(
+                        "{}\t{}\t{}\t{}\t{}",
+                        record.chrom,
+                        p,
+                        "-",
+                        complement_base_code(r.as_bytes()[0]) as char,
+                        val
+                    );
+                }
+            } else {
+                let passed_filter = (0..bam_path_list.len())
+                    .map(|x| match p2depth.get(&(p, x)) {
+                        Some(val) => (*val).0 + (*val).1,
+                        None => 0,
+                    })
+                    .max()
+                    .unwrap()
+                    >= min_depth;
+                if passed_filter {
+                    let val = rec_list.iter().map(|x| &x[0]).join("\t");
+                    println!("{}\t{}\t{}\t{}\t{}", record.chrom, p, "+/-", r, val);
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod base;
 mod count;
 
 use clap::Parser;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 #[clap(about, version, author)]
@@ -33,16 +33,31 @@ struct Base {
         long = "input",
         help = "input bam files..",
         required = true,
-        parse(from_os_str)
+        parse(from_os_str),
+        takes_value = true, multiple_values = true, validator = file_path_validation,
     )]
     bam: Vec<PathBuf>,
     #[clap(
         short = 'd',
         long = "min-depth",
-        help = "Set min mean depth for output",
+        help = "Set min depth for output. Note: anyone of the samples passing the cutoff is ok.",
         default_value = "0"
     )]
     depth: u32,
+    #[clap(short = 'w', long = "with-header", help = "Write header in the output")]
+    header: bool,
+    #[clap(
+        short = 'S',
+        long = "strandless",
+        help = "Calcualte counts ignore strand info"
+    )]
+    strandless: bool,
+    #[clap(
+        short = 's',
+        long = "split-strand",
+        help = "Split counts into different rows by strand"
+    )]
+    bystrand: bool,
 }
 
 #[derive(Parser)]
@@ -75,10 +90,30 @@ fn main() {
     // (as below), requesting just the name used, or both at the same time
     match opts.subcmd {
         SubCommand::Base(o) => {
-            base::run(o.bed, o.fa, o.bam, o.depth, o.indel);
+            base::run(
+                o.bed,
+                o.fa,
+                o.bam,
+                o.depth,
+                o.indel,
+                o.header,
+                o.strandless,
+                o.bystrand,
+            );
         }
         SubCommand::Count(o) => {
             count::run(o.bed, o.fa, o.bam);
         }
+    }
+}
+
+fn file_path_validation(path: &str) -> Result<(), String> {
+    let path = Path::new(path);
+    if !path.exists() {
+        Err(format!("{path:?} file doesn't exists"))
+    } else if !path.is_file() {
+        Err(format!("{path:?} is not a file"))
+    } else {
+        Ok(())
     }
 }
