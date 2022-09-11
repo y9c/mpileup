be639edff94cc7cda3d2cb75e45ecb879dc18011
Chang Ye <yech1990@gmail.com>
2022-09-10 22:15:40 -0500

    update workflow

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+#
+# Copyright © 2022 Ye Chang yech1990@gmail.com
+# Distributed under terms of the GNU license.
+#
+# Created: 2022-09-10 22:13
+
+sudo apt-get update && sudo apt-get -y install gcc pkg-config libssl-dev openssl make cmake
